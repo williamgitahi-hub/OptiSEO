ec872b0aac286519ee43cfde7737c9bda44b8653
Will William <williamgitahi@gmail.com>
2026-04-24 18:33:50 +0300

    fix cors for netlify frontend

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ app = FastAPI()
 # Allow frontend (React) to talk to backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://optiqo-frontend.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
